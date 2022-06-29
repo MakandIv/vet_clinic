f84fcacb71d80a3d50316b85ff4cce976cf85e5e
MakandIv <makushkin_andrey@mail.ru>
2022-06-29 21:13:34 +0400

    mini update exception

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
             System.out.println("The program has ended.");
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("Database connection error.");
         }
     }
 
