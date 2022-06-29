31cba90f1f374d466e38e70f3bf71bc6475f5950
MakandIv <makushkin_andrey@mail.ru>
2022-06-29 21:31:01 +0400

    Added properties.

--- src/JDBCConnection.java
+++ src/JDBCConnection.java
@@ -1,6 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * The class responsible for connecting to the database. The connection is performed by the "connect" method.
@@ -13,9 +18,23 @@ public class JDBCConnection {
      * @throws SQLException if an error occurs during connection.
      */
     public static Connection connect() throws SQLException {
-        String jdbcURL = "jdbc:postgresql://localhost:5433/vet_clinic";
-        String login = "postgres";
-        String password = "adminad";
+        Properties properties = new Properties();
+        String jdbcURL = null;
+        String login = null;
+        String password = null;
+        try {
+            properties.load(new FileReader("jdbc.properties"));
+            jdbcURL = properties.getProperty("jdbcURL");
+            login = properties.getProperty("login");
+            password = properties.getProperty("password");
+        } catch (IOException ignored) {}
+
+        if (jdbcURL == null || login == null || password == null) { // Hardcoded data if jbdc.properies not exist.
+            jdbcURL = "jdbc:postgresql://localhost:5433/vet_clinic";
+            login = "postgres";
+            password = "adminad";
+        }
+
         return DriverManager.getConnection(jdbcURL, login, password);
     }
 }
